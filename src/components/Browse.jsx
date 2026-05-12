c9ee671ccc1de37439d622b803749d73330e8e42
Shivani Kumari <shivani13222@gmail.com>
2026-05-12 14:16:25 +0530

    api call - get data from TMDB now playing movies list api

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './Header';
+import { API_OPTIONS, TMDB_BASE_URL } from '../utils/constants';
 
 const Browse = () => {
+
+  
+
+  const getNowPlayingMovies = async() =>{
+    try {
+      const response = await fetch(TMDB_BASE_URL, API_OPTIONS);
+      const data = await response.json();
+
+      console.log("Now Playing Movies:", data.results);
+
+    }catch (error) {
+      console.log("Error fetching now playing movies:", error);
+    } 
+  }
+
+  useEffect(() => {
+    getNowPlayingMovies();
+  }, []);
+
+
+
   return (
     <div>
       <Header/>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,18 +2,17 @@ import logo from "../assets/images/Netflix_Logo.png";
 
 export const LOGO = logo;
 
-export const USER_AVATAR =  "https://avatars.githubusercontent.com/u/123661346?v=4";
+export const USER_AVATAR =
+  "https://avatars.githubusercontent.com/u/123661346?v=4";
 
-const url = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1';
-const options = {
-  method: 'GET',
+
+  //api options for fetching data from tmdb api
+export const TMDB_BASE_URL =  "https://api.themoviedb.org/3/movie/now_playing?page=1";
+ ;
+export const API_OPTIONS = {
+  method: "GET",
   headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMjQ1YTkxODkxYWM2NDQ2MzI2Y2EwMDRlZGRmZDQ1NSIsIm5iZiI6MTc3ODU2OTAyMi4xNzI5OTk5LCJzdWIiOiI2YTAyY2YzZWFlMmI2MjhkYzNlMTU5ZTciLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.fTFpBcjJtkv_O0izh_3WihyL7FZATuS8OlyuaRUnpfU'
-  }
+    accept: "application/json",
+    Authorization: `Bearer ${process.env.REACT_APP_TMDB_ACCESS_TOKEN}`,
+  },
 };
-
-fetch(url, options)
-  .then(res => res.json())
-  .then(json => console.log(json))
-  .catch(err => console.error(err));
