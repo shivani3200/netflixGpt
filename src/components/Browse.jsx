c4f1682b911f2ceff9e78c70f09755e12d64b284
Shivani Kumari <shivani13222@gmail.com>
2026-05-12 15:22:43 +0530

    created movies slice and dispatch it i.e. stored the movies data in redux store

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,18 +1,21 @@
 import React, { useEffect } from 'react'
 import Header from './Header';
 import { API_OPTIONS, TMDB_BASE_URL } from '../utils/constants';
+import { useDispatch } from 'react-redux';
+import { addNowPlayingMovies } from '../utils/moviesSlice';
 
 const Browse = () => {
+  const dispatch = useDispatch();
 
-  
 
   const getNowPlayingMovies = async() =>{
     try {
       const response = await fetch(TMDB_BASE_URL, API_OPTIONS);
       const data = await response.json();
-
       console.log("Now Playing Movies:", data.results);
-
+      // Dispatch the action to store now playing movies in Redux
+      dispatch( addNowPlayingMovies(data.results) );
+ 
     }catch (error) {
       console.log("Error fetching now playing movies:", error);
     } 
