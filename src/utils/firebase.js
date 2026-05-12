c7f4ae012c20735877b37251af198e677cd146b3
Shivani Kumari <shivani13222@gmail.com>
2026-05-12 13:44:16 +0530

    added env and env.example file

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,14 +8,15 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCIAJpX9TQ8BeE1uZZUjJs-Kd55e_ADvP0",
-  authDomain: "netflixgpt-shiv.firebaseapp.com",
-  projectId: "netflixgpt-shiv",
-  storageBucket: "netflixgpt-shiv.firebasestorage.app",
-  messagingSenderId: "420323000504",
-  appId: "1:420323000504:web:a1eb19cc8e1d60b5b7744d",
-  measurementId: "G-X75203PJM8"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
